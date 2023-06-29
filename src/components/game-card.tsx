8ef76430885352c6b7936dd8d8ef8c63e0fabfa3
jhrhq <jhr.code@proton.me>
2023-06-30 00:24:15 +0600

    platform icons

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -1,5 +1,7 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hook/use-games";
+import PlatformIconList from "./platform-icon-list";
+
 interface Props {
   game: Game;
 }
@@ -9,6 +11,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
