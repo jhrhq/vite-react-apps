1f24b77f350338d46d30ef4efc24cfe5c8ca8626
jhrhq <jhr.code@proton.me>
2023-06-29 18:50:15 +0600

    api error fixed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api/games",
+  baseURL: "https://api.rawg.io/api/",
   params: {
     key: "854e4cbf7b0c45908cbc5f305a4c21d9",
   },
