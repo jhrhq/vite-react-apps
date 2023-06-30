67546731ecbcd953f4aa0ae785b877050e569285
jhrhq <jhr.code@proton.me>
2023-06-30 16:11:13 +0600

    genre list ui

--- src/components/genre-lists.tsx
+++ src/components/genre-lists.tsx
@@ -1,16 +1,31 @@
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
 import useGenres from "../hook/use-genres";
+import getCroppedImageUrl from "../services/image-urls";
 
 const GenreLists = () => {
   const { data: genres, isLoading, error } = useGenres();
   if (error) return null;
   return (
-    <div>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
-    </div>
+    <List>
+      {genres.map((genre) => (
+        <ListItem key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              onClick={() => console.log(genre)}
+              fontSize={"lg"}
+              variant={"link"}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
