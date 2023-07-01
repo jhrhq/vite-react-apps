8e7ceae72360d7918718f0252311e8cdb58075a2
jhrhq <jhr.code@proton.me>
2023-07-01 16:05:11 +0600

    cleanup the game cards

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -13,12 +13,12 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
-    <div>
+    <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={"10px"}
-        spacing={10}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -32,7 +32,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </div>
+    </>
   );
 };
 
