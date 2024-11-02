b54a63dbd5612175166513dbd74bbd217a4cb72a
jhrhq <jhr.code@proton.me>
2024-11-03 02:43:33 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+        primary: "#00D991",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
