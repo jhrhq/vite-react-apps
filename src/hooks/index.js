3cdde5e0a2b7c0f38e57649108307379d19b1080
jhrhq <jhr.code@proton.me>
2024-11-05 01:25:26 +0600

    fix: hooks import path

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-import { useDebounce } from "@/hooks/useDebounce";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
+import useDebounce from "@/hooks/useDebounce";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export { useDebounce, useLocalStorage };
