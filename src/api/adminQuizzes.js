1928c6d8a10c05af3fbaf0ceee83dd703b5ed5be
jhrhq <jhr.code@proton.me>
2024-11-22 09:29:15 +0600

    feat: update question

--- src/api/adminQuizzes.js
+++ src/api/adminQuizzes.js
@@ -3,7 +3,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const adminQuizzesApi = createApi({
   reducerPath: "adminQuizzesApi",
-  tagTypes: ["Quizzes"],
+  tagTypes: ["adminQuizzes"],
   baseQuery: axiosBaseQuery(),
   endpoints: (builder) => ({
     getAdminQuizzes: builder.query({
@@ -21,9 +21,67 @@ export const adminQuizzesApi = createApi({
           data,
         };
       },
+      invalidatesTags: ["adminQuizzes"],
+    }),
+    addQuestion: builder.mutation({
+      query: ({ quizId, ...rest }) => {
+        return {
+          url: `/api/admin/quizzes/${quizId}/questions`,
+          method: "POST",
+          data: { ...rest.questionData },
+        };
+      },
+      invalidatesTags: ["adminQuizzes"],
+    }),
+    updateQuiz: builder.mutation({
+      query: ({ quizId, ...rest }) => {
+        return {
+          url: `/api/admin/quizzes/${quizId}/questions`,
+          method: "PATCH",
+          data: { ...rest.questionData },
+        };
+      },
+      invalidatesTags: ["adminQuizzes"],
+    }),
+    updateQuestion: builder.mutation({
+      query: ({ questionId, ...rest }) => {
+        return {
+          url: `/api/admin/questions/${questionId}`,
+          method: "PATCH",
+          data: { ...rest.questionData },
+        };
+      },
+      invalidatesTags: ["adminQuizzes"],
+    }),
+    removeQuestion: builder.mutation({
+      query: ({ quizId }) => {
+        return {
+          url: `/api/admin/questions/${quizId}`,
+          method: "DELETE",
+          // data: { ...rest.questionData },
+        };
+      },
+      invalidatesTags: ["adminQuizzes"],
+    }),
+    removeQuiz: builder.mutation({
+      query: ({ quizId }) => {
+        return {
+          url: `/api/admin/questions/${quizId}`,
+          method: "DELETE",
+          // data: { ...rest.questionData },
+        };
+      },
+      invalidatesTags: ["adminQuizzes"],
     }),
   }),
 });
 
-export const { useGetAdminQuizzesQuery, useCreateQuizMutation } =
-  adminQuizzesApi;
+export const {
+  useGetAdminQuizzesQuery,
+  useCreateQuizMutation,
+  useAddQuestionMutation,
+  useUpdateQuestionMutation,
+  useUpdateQuizMutation,
+  useRemoveQuestionMutation,
+  useRemoveQuizMutation,
+} = adminQuizzesApi;
