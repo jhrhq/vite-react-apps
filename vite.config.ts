1ed3c792e14ca3a4cbd7db55135f509b010359e0
jhrhq <jhr.code@proton.me>
2026-06-18 12:36:54 +0600

    chore: initial commit with vite setup

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // 2. Define your alias and map it to the physical directory
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
